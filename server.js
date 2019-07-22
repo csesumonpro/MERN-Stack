add2de789749193f6eb4fb4ae3119f125fb83e66
Sumon sarker <csesumonpro@gmail.com>
2019-07-22 13:19:09 +0600

    mongodb connection added

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+connectDB();
 app.get('/',(req,res)=>res.send('API Running...'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>`Server started on PORT ${PORT}`)
