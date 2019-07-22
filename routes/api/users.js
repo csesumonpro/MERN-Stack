20efa5da3c80ad1b02d75370dfb27ac755ca9b62
Sumon sarker <csesumonpro@gmail.com>
2019-07-23 03:09:07 +0600

    express-validator form validation system added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
-//@route api/users
-//@desc Test route
+// Validator
+const { check, validationResult } = require('express-validator');
+
+//@route Post api/users
+//@desc Register User
 //@access Public
 
-router.get('/',(req,res)=>res.send('User Route'));
+router.post('/',[
+    check('name','Name is Required')
+    .not()
+    .isEmpty(),
+    check('email','Please Enter a Valid Email').isEmail(),
+    check('password','Please enter at least 6 character').isLength(6)
+],(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({errors:errors.array()});
+    }
+    res.send('User Route')
+});
 
 module.exports = router;
