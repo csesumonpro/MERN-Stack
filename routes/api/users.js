ba0b5564475f602a77b78b34efb703fafa52b701
Sumon sarker <csesumonpro@gmail.com>
2019-07-25 01:59:12 +0600

    JWT added

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const gravatar =  require('gravatar');
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
-
+const jwt = require('jsonwebtoken');
+const config =  require('config');
 // Validator
 const { check, validationResult } = require('express-validator');
 
@@ -25,10 +26,11 @@ async (req,res)=>{
     }
     const {name,email,password} = req.body;
     try {
-        let user = User.findOne({email});
-        if(user){
-            return res.status(500).json({"errors":[{"msg":'User ALready Exist!'}]});
-        }
+        // let user = User.findOne({email});
+      
+        // if(user){
+        //     return res.status(500).json({"errors":[{"msg":'User ALready Exist!'}]});
+        // }
         const avatar = gravatar.url(email,{
             s:'200',
             r:'pg',
@@ -40,7 +42,19 @@ async (req,res)=>{
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password,salt);
         await user.save();
-        res.send('User Registered');
+        const payload = {
+           user:{
+            id:user.id
+           }
+        }
+        jwt.sign(payload,
+            config.get('jwtSecret'),
+            {expiresIn:3600},
+            (err,token)=>{
+             if(err) throw err   
+             res.json({token})
+        })
+       
     } catch (error) {
         console.log(error.message);
         res.status(500).send('Server Error');
