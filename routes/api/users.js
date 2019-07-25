b8c89b2e900f025ddd24ac3fb9909a2a1ff39d00
Sumon sarker <csesumonpro@gmail.com>
2019-07-25 12:01:50 +0600

    auth middleware added

--- routes/api/users.js
+++ routes/api/users.js
@@ -49,7 +49,7 @@ async (req,res)=>{
         }
         jwt.sign(payload,
             config.get('jwtSecret'),
-            {expiresIn:3600},
+            {expiresIn:36000},
             (err,token)=>{
              if(err) throw err   
              res.json({token})
