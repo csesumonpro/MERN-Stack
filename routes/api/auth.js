c9c66a4c6b9fa1c665fdd4cba6ae4f489be5c91f
Sumon sarker <csesumonpro@gmail.com>
2019-07-25 12:54:29 +0600

    User authentication login system added

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+// Validator
+const { check, validationResult } = require('express-validator');
 //@route api/auth
-//@desc Test route
-//@access Public
+//@desc Auth route
+//@access Private
 
 router.get('/',auth,async(req,res)=>{
     try {
@@ -15,5 +21,48 @@ router.get('/',auth,async(req,res)=>{
         res.status(500).send('Server Error');
     }
 });
+
+router.post('/',[
+    
+    check('email','Please Enter a Valid Email').isEmail(),
+    check('password','Please enter at least 6 character').exists()
+],
+async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({errors:errors.array()});
+    }
+    const {email,password} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(500).json({"errors":[{"msg":'Invalid Credential!'}]});
+        }
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(500).json({"errors":[{"msg":'Invalid Credential!'}]});
+        }
+        const payload = {
+           user:{
+            id:user.id
+           }
+        }
+      
+        jwt.sign(payload,
+            config.get('jwtSecret'),
+            {expiresIn:36000},
+            (err,token)=>{
+             if(err) throw err   
+             res.json({token})
+        })
+     
+       
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Server Error");
+    }
+    
+});
+
 
 module.exports = router;
